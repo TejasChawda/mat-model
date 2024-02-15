c84af5a6935e8b6b25f4421593f8f9d1d50c426d
TejasChawda <tejasc@testvagrant.com>
2024-02-15 15:38:44 +0530

    paths changed

--- src/database.py
+++ src/database.py
@@ -13,7 +13,7 @@ document_ids = []
 def init_db():
     if not firebase_admin._apps:
         cred = credentials.Certificate(
-            '/Users/admin/Desktop/pythonStreamlitDemo/Config/testapp-20a32-firebase-adminsdk-qf29b-35e854714d.json')
+            'config/testapp-20a32-firebase-adminsdk-qf29b-35e854714d.json')
         firebase_admin.initialize_app(cred)
 
 
